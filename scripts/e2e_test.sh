48bff10d03dafaea2ed342195948f01e108ddd33
Nishant Bansal <nishant.bansal.282003@gmail.com>
2025-05-17 19:30:19 +0530

    chore: update e2e tests
    Signed-off-by: Nishant Bansal <nishant.bansal.282003@gmail.com>

--- scripts/e2e_test.sh
+++ scripts/e2e_test.sh
@@ -7,11 +7,27 @@ VOLUME_MOUNTS_CMD="-v ./temp:/app/fuzz_results"
 # Create the temp directory if it doesn't exist
 mkdir -p ./temp
 
-# Run the make command with a 60-minute timeout
-timeout 60m make docker-run-file ENV_FILE="$ENV_FILE_NAME" VOLUME_MOUNTS="$VOLUME_MOUNTS_CMD"
+# Start the make command in the background
+make docker-run-file ENV_FILE="$ENV_FILE_NAME" VOLUME_MOUNTS="$VOLUME_MOUNTS_CMD" &
+MAKE_PID=$!
+
+# After 60 minutes, send SIGINT (Ctrl+C) to the make process
+(
+  sleep 60m
+  echo "⏰ 60 minutes elapsed, sending SIGINT to make (PID $MAKE_PID)..."
+  kill -INT $MAKE_PID
+) &
+
+WAITER_PID=$!
+
+# Wait for the make command to finish
+wait $MAKE_PID
 EXIT_STATUS=$?
 
-# Check the exit status of the timeout command
+# Kill the waiter if make finished early
+kill $WAITER_PID 2>/dev/null
+
+# Check the exit status of the make command
 if [ $EXIT_STATUS -ne 0 ]; then
   echo "❌ The operation exited with status $EXIT_STATUS."
   exit $EXIT_STATUS
